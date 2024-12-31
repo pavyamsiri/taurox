8b21a1310236c2d8a9cbc40fc7ee20cdb2d68581
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-12-31 20:06:32 +1100

    tests: Add print tests

--- tests/interpreter_test.rs
+++ tests/interpreter_test.rs
@@ -135,6 +135,12 @@ fn test_number() -> Result<()> {
     test_engine(input_dir)
 }
 
+#[test]
+fn test_print() -> Result<()> {
+    let input_dir = Path::new("./test_data/interpreter/print");
+    test_engine(input_dir)
+}
+
 struct TestCase {
     name: String,
     source: String,
